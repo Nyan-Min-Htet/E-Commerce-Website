1554405b541b4d85a0ccfeb8b1470cc373f1fd4f
Nyan-Min-Htet <nhtet9968@gmail.com>
2025-03-24 21:48:14 +0630

    first commit

--- customer/navbar.php
+++ customer/navbar.php
@@ -8,6 +8,7 @@ if (isset($_SESSION['email'])) {
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -28,48 +29,50 @@ if (isset($_SESSION['email'])) {
     }
     ?>
     <style>
-       .badge{
-            position: absolute;
-            font-size: 18px;
-            font-weight: bold;
-            top: 48px;
-        }
+    .badge {
+        position: absolute;
+        font-size: 18px;
+        font-weight: bold;
+        top: 48px;
+    }
     </style>
 </head>
+
 <body>
-<nav>
-    <div class="nav-container">
-        <div class="search_icon">
-            <form action="search.php" method="get">
-                <input type="text" placeholder="Search....." name="search" class="search">
-                <button type="submit" style="border:none; background-color: white; height: 60px;">
-                    <i class="fa fa-search" style="right: -10px; width: 20px; top: 30px;"></i>
-                </button>
-            </form>
-        </div>
-        <div class="logo">
+    <nav>
+        <div class="nav-container">
+            <div class="search_icon">
+                <form action="search.php" method="get">
+                    <input type="text" placeholder="Search....." name="search" class="search">
+                    <button type="submit" style="border:none; background-color: white; height: 60px;">
+                        <i class="fa fa-search" style="right: -10px; width: 20px; top: 30px;"></i>
+                    </button>
+                </form>
+            </div>
+            <div class="logo">
                 <a href="index.php">
                     <img src="../img/logo.png" alt="logo">
                 </a>
-        </div>
-        <div class="login" style="position: relative;">
+            </div>
+            <div class="login" style="position: relative;">
                 <!-- <a href="favorite.php">
                     <i class="fa-regular fa-heart"></i>
                 </a> -->
                 <?php if (isset($_SESSION['email'])): ?>
-                        <?php if ($_SESSION['role'] === 'admin'): ?>
-                            <!-- If user is an admin -->
-                            <a href="admin_dashboard.php"><button class="login_btn">Admin View</button></a>
-                            <a href="logout.php"><button class="login_btn">Logout</button></a>
-                        <?php else: ?>
-                            <!-- If user is a regular customer -->
-                            <!-- <a href="my_account.php">My Account</a> -->
-                            <a href="logout.php"><button class="login_btn">Logout</button></a>
-                        <?php endif; ?>
-                    <?php else: ?>
-                        <!-- If user is not logged in -->
-                        <a href="login.php"><button class="login_btn" style="width: 20%;">Log In</button></a>
-                        <a href="sign_up.php"><button class="login_btn" style="width: 23%; margin-left: 15px;">Register</button></a>
+                <?php if ($_SESSION['role'] === 'admin'): ?>
+                <!-- If user is an admin -->
+                <a href="admin_dashboard.php"><button class="login_btn">Admin View</button></a>
+                <a href="logout.php"><button class="login_btn">Logout</button></a>
+                <?php else: ?>
+                <!-- If user is a regular customer -->
+                <!-- <a href="my_account.php">My Account</a> -->
+                <a href="logout.php"><button class="login_btn">Logout</button></a>
+                <?php endif; ?>
+                <?php else: ?>
+                <!-- If user is not logged in -->
+                <a href="login.php"><button class="login_btn" style="width: 20%;">Log In</button></a>
+                <a href="sign_up.php"><button class="login_btn"
+                        style="width: 23%; margin-left: 15px;">Register</button></a>
                 <?php endif; ?>
                 <a href="login.php">
                     <i class="fa-regular fa-user"></i>
@@ -77,12 +80,13 @@ if (isset($_SESSION['email'])) {
                 <a href="cart.php">
                     <i class="fa fa-cart-shopping"></i>
                     <?php if ($total_items > 0): ?>
-                        <span class="badge"><?= $total_items ?></span>
+                    <span class="badge"><?= $total_items ?></span>
                     <?php endif; ?>
                 </a>
+            </div>
         </div>
-    </div>
-</nav>
+    </nav>
 
 </body>
+
 </html>
